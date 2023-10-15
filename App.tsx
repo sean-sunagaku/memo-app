81d1fe176c2362b7b3b8cbd74aa26db54d2f7676
Shunsuke <shunsuke.au.softbank@gmail.com>
2023-10-15 12:00:32 +0900

    add HelloComponentの修正

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Hello from './src/components/Hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!!!</Text>
       <StatusBar style="auto" />
+      <Hello bang={true} style={{ fontSize: 16 }}>
+        World
+      </Hello>
+      <Hello bang={true}>World</Hello>
     </View>
   );
 }
