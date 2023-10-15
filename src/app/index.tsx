59dd263828e0424483226c6095139c7d1018a094
Shunsuke <shunsuke.au.softbank@gmail.com>
2023-10-15 13:31:46 +0900

    add テンプレートの作成

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const index = (): JSX.Element => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default index;
