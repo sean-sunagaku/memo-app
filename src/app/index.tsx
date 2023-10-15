f2196656a05a34ab89f3169642d66d2536eebd18
Shunsuke <shunsuke.au.softbank@gmail.com>
2023-10-15 13:41:33 +0900

    add indexの構造化完了

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,56 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const index = (): JSX.Element => {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <View>
+        <Text>Memo App</Text>
+        <Text>ログアウト</Text>
+      </View>
+
+      <View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023年10月1日 10:00</Text>
+            <View>
+              <Text>×</Text>
+            </View>
+          </View>
+        </View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023年10月1日 10:00</Text>
+            <View>
+              <Text>×</Text>
+            </View>
+          </View>
+        </View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023年10月1日 10:00</Text>
+            <View>
+              <Text>×</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default index;
